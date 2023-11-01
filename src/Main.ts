fbefdc011eea700f0fb62a5f59953d7322c977c7
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-11-01 20:50:07 +0000

    Updated error handling in the 'unhandledRejection' event to better identify non-error rejections and improved error reporting on failed embed message sends.

--- src/Main.ts
+++ src/Main.ts
@@ -23,8 +23,8 @@ const client = new FusionClient({
  * @param error - The error that was not handled.
  * @returns void
  */
-process.on('unhandledRejection', (error: Error) => {
-    if (!error?.stack) return;
+process.on('unhandledRejection', async (error) => {
+    if (!error || !(error instanceof Error) || !error.stack) return;
 
     console.error(error.stack);
 
@@ -49,7 +49,12 @@ process.on('unhandledRejection', (error: Error) => {
         }
 
         const embed = new EmbedBuilder().setTitle('Error').setDescription(truncateDescription(fullString));
-        channel.send({ embeds: [embed] });
+
+        try {
+            await channel.send({ embeds: [embed] });
+        } catch (sendError) {
+            console.error('An error occurred while sending the error embed:', sendError);
+        }
     }
 });
 
